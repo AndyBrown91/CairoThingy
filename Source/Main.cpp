4877aac56a624f5a86e6df5a5ebf9fc072d73aab
Andy Brown <andyjtbrown@gmail.com>
2020-12-21 12:18:50 +0000

    Initial commit

---
+++ Source/Main.cpp
@@ -0,0 +1,44 @@
+/*
+  ==============================================================================
+
+    This file contains the basic startup code for a JUCE application.
+
+  ==============================================================================
+*/
+
+#include <JuceHeader.h>
+#include <cairo/cairo.h>
+#include <cairo/cairo-pdf.h>
+
+//==============================================================================
+int main (int argc, char* argv[])
+{
+    const juce::File output (juce::File::getSpecialLocation (juce::File::currentExecutableFile).getSiblingFile ("test.pdf"));
+
+    if (output.existsAsFile())
+        output.deleteFile();
+
+    // Creating a cairo PDF Surface
+    cairo_surface_t *csurface = cairo_pdf_surface_create(output.getFullPathName().toRawUTF8(), 500, 400);
+    // Creating a cairo context
+    cairo_t *ctx = cairo_create(csurface);
+    // Creating rectangle in PDF
+    cairo_rectangle(ctx, 0.0, 0.0, 400, 300);
+    // Changing rectangle bacground color to Blue
+    cairo_set_source_rgb(ctx, 0.0, 0.0, 0.5);
+    cairo_fill(ctx);
+    // Moving to (10, 10) position in PDF
+    cairo_move_to(ctx, 10.0, 10.0);
+    // Changing text color to Yellow
+    cairo_set_source_rgb(ctx, 1.0, 1.0, 0.0);
+    // Writing some text to PDF
+    cairo_show_text(ctx, "This is a test");
+    cairo_show_page(ctx);
+    // Destroying cairo context
+    cairo_destroy(ctx);
+    cairo_surface_flush(csurface);
+    // Destroying PDF surface
+    cairo_surface_destroy(csurface);
+
+    return 0;
+}
---
+++ get_cairo.py
@@ -0,0 +1,90 @@
+from pathlib import Path
+import sys
+import os
+from subprocess import Popen, PIPE, STDOUT
+
+
+def get_script_dir():
+    return Path(sys.path[0])
+
+
+def is_windows():
+    if not os.name == "nt":
+        return "cygdrive" in os.getcwd()
+
+    return True
+
+
+def run_cmd(cmd, exit_on_failure=True, hide_output=False):
+    if exit_on_failure:
+        if "|" in cmd:
+            cmd = cmd + "; test ${PIPESTATUS[0]} -eq 0"
+        else:
+            cmd = cmd + " || exit 1"
+
+    try:
+        lines = []
+
+        with Popen(cmd, stdout=PIPE, stderr=STDOUT, bufsize=1, shell=True) as p:
+            for line in p.stdout:
+                line = line.decode('utf-8')
+
+                if not hide_output:
+                    print(line, end='')
+                lines.append(line)
+
+        if not p.returncode == 0 and exit_on_failure:
+            print(cmd + " - Returned: " + str(p.returncode))
+            exit(1)
+
+        return [p.returncode, lines]
+    except:
+        print("Error executing cmd: " + cmd)
+
+        if exit_on_failure:
+            exit(1)
+
+        return None
+
+
+def check_application_exists(name):
+    """Check whether `name` is on PATH and marked as executable."""
+    from shutil import which
+
+    return which(name) is not None
+
+
+if not check_application_exists("git"):
+    raise RuntimeError("Couldn't find git")
+
+vcpkg_path = get_script_dir() / "vcpkg"
+
+if not vcpkg_path.exists():
+    print("-- Cloning vcpkg")
+    run_cmd(f"git clone https://github.com/microsoft/vcpkg.git")
+
+    for dirpath, dirnames, filenames in os.walk(vcpkg_path):
+        os.chmod(dirpath, 0o777)
+        for filename in filenames:
+            os.chmod(os.path.join(dirpath, filename), 0o777)
+
+extension = ".exe" if is_windows() else ""
+vcpkg_exe = vcpkg_path / str("vcpkg" + extension)
+
+if not vcpkg_exe.exists():
+    print("-- Building vcpkg")
+    bootstrap = "./vcpkg/bootstrap-vcpkg.bat" if is_windows() else "./vcpkg/bootstrap-vcpkg.sh"
+    run_cmd(f"{get_script_dir() / bootstrap}")
+
+print("-- Downloading required libraries")
+libraries = ["cairo"]
+triplet = "x64-windows-static" if is_windows() else "x64-osx"
+
+for library in libraries:
+    run_cmd(f"{vcpkg_exe} install {library} --triplet={triplet}")
+
+libs_path = get_script_dir() / "external_libs"
+
+if not libs_path.exists():
+    os.symlink(vcpkg_path / "installed" / triplet, libs_path)
+
